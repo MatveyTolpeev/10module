4eec9b492a32fed3e8193abd848d4403e83be27e
MatveyTolpeev <61470886+MatveyTolpeev@users.noreply.github.com>
2020-11-20 01:11:51 +0500

    Update Runner.java

--- 10module/messaging-rabbitmq-master/src/main/java/com/example/messagingrabbitmq/Runner.java
+++ 10module/messaging-rabbitmq-master/src/main/java/com/example/messagingrabbitmq/Runner.java
@@ -13,12 +13,7 @@ public class Runner implements CommandLineRunner {
 
 	private final RabbitTemplate rabbitTemplate;
 	private final Receiver receiver;
-
-	public static final String ANSI_RESET = "\u001B[0m";
-
-	private static final String[] colors = new String[] {"\u001B[31m","\u001B[32m",
-														 "\u001B[33m", "\u001B[34m",
-														 "\u001B[35m", "\u001B[36m"};
+	
 
 
 	public Runner(Receiver receiver, RabbitTemplate rabbitTemplate) {
@@ -29,16 +24,16 @@ public class Runner implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		Scanner sc = new Scanner(System.in);
-		String color = colors[new Random().nextInt(colors.length - 1)];
-		System.out.println(colors[1] + "\n\n\nEnter the message or wait for it" + ANSI_RESET);
+		
+		System.out.println("\n\n\nEnter the message or wait for it");
 		while(true) {
 			String message = sc.nextLine();
-			System.out.println(colors[0] + ">sending message..." + ANSI_RESET);
+			System.out.println(">sending message...");
 			rabbitTemplate.convertAndSend(
 					MessagingRabbitmqApplication.exchangeName,
 					"com.matvey.testMessage",
-					color + message + ANSI_RESET);
-			System.out.println(colors[5] + ">message sent" + ANSI_RESET);
+					message );
+			System.out.println(">message sent");
 			receiver.getLatch().await(1000, TimeUnit.MILLISECONDS);
 		}
 	}
